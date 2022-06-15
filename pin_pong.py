19938db7173904afce46d273c3b105de4aef2816
Salimov Karim <Salimov.karim00@gmail.com>
2022-06-15 15:13:52 +0500

    add commit

--- pin_pong.py
+++ pin_pong.py
@@ -27,8 +27,7 @@ class Player(GameSprite):
             self.rect.y -= self.speed
         if keys[K_s] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
-#sjrfgjdgfdjuhgu
-
+#mghmkfgh
 
 
 win_width = 700
